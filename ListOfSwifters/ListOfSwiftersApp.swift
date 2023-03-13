2027c8279519399bed48dae8ff4658e9f6960881
Astrid Snäll <vaski2astrid@hotmail.com>
2023-03-13 17:18:23 -0400

    Initial Commit

---
+++ ListOfSwifters/ListOfSwiftersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfSwiftersApp.swift
+//  ListOfSwifters
+//
+//  Created by Astrid Snäll on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ListOfSwiftersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
